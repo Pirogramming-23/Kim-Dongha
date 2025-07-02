72397e2a48655f4113f05a443ab5affbdc64e4c5
eastha10 <kdh9158kdh@gmail.com>
2025-07-02 18:03:16 +0900

    JavaScript - fetch

---
+++ Javascript 쇼핑몰 미니게임 과제/src/shopping.js
@@ -0,0 +1,17 @@
+
+
+// Fetch the items from the JSON file
+function loadItems(){
+    return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => console.log(json));
+}
+
+//main
+loadItems()
+    .then(items => {
+        console.log(items);
+        // displayItems(items);
+        // setEventListners(items)
+    })
+    .catch(console.log);
