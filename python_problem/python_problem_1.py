6a222996a098a7a9e54f42e6cac92c320d838282
eastha10 <kdh9158kdh@gmail.com>
2025-07-03 19:42:51 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,25 +1,34 @@
 num = 0 
 while True:
-    try:
-        play = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
-        if play > 3 or play < 1:
-            print("1, 2, 3중 하나를 입력하세요")
-            continue
-        break
-    except ValueError:
-        print("정수를 입력하세요")
-for i in range(play):
-    num += 1
-    print(f"playerA:{num}")
-while True:
-    try:
-        play = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
-        if play > 3 or play < 1:
-            print("1, 2, 3중 하나를 입력하세요")
-            continue
-        break
-    except ValueError:
-        print("정수를 입력하세요")
-for i in range(play):
-    num += 1
-    print(f"playerB:{num}")
+    while True:
+        try:
+            play = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
+            if play > 3 or play < 1:
+                print("1, 2, 3중 하나를 입력하세요")
+                continue
+            break
+        except ValueError:
+            print("정수를 입력하세요")
+    for i in range(play):
+        num += 1
+        if num > 31:
+            break
+        print(f"playerA:{num}")
+    if num > 31:
+            break
+    while True:
+        try:
+            play = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
+            if play > 3 or play < 1:
+                print("1, 2, 3중 하나를 입력하세요")
+                continue
+            break
+        except ValueError:
+            print("정수를 입력하세요")
+    for i in range(play):
+        num += 1
+        if num > 31:
+            break
+        print(f"playerB:{num}")
+    if num > 31:
+            break
