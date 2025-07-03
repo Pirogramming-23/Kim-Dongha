960ce4f3b1f3759163403f5955374b844509d5a4
eastha10 <kdh9158kdh@gmail.com>
2025-07-03 19:59:51 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,5 +1,7 @@
+import random
 num = 0 
-def brGame(player1, player2):
+
+def brGame():
     global num
     while True:
         try:
@@ -12,18 +14,29 @@ def brGame(player1, player2):
             print("정수를 입력하세요")
     for i in range(play):
         num += 1
-        if num > 31:
-            print(f"player{player2} win!")
-            break
-        print(f"player{player1}:{num}")
-    return
+        print(f"player:{num}")
+        if num >= 31:
+            print(f"computer win!")
+            return True 
+    return False
+
+def computer():
+    global num
+    ans = random.randint(1, 3)
+    for i in range(ans):
+        num += 1
+        print(f"computer:{num}")
+        if num >= 31:
+            print(f"player win!")
+            return True 
+    return False
 
 cnt = 0
 while True:
     if cnt % 2 == 0:
-        brGame('A', 'B')
-    else:
-        brGame('B', 'A')
-    if num > 31:
+        if brGame():
             break
-    cnt+=1
+    else:
+        if computer():
+            break
+    cnt += 1
