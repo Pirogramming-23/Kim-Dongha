f5a508dc21113a9ae27e46c24cdf294fad26e3d2
eastha10 <kdh9158kdh@gmail.com>
2025-07-03 19:52:17 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,36 +1,29 @@
 num = 0 
+def brGame(player1, player2):
+    global num
+    while True:
+        try:
+            play = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
+            if play > 3 or play < 1:
+                print("1, 2, 3중 하나를 입력하세요")
+                continue
+            break
+        except ValueError:
+            print("정수를 입력하세요")
+    for i in range(play):
+        num += 1
+        if num > 31:
+            print(f"player{player2} win!")
+            break
+        print(f"player{player1}:{num}")
+    return
+
+cnt = 0
 while True:
-    while True:
-        try:
-            play = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
-            if play > 3 or play < 1:
-                print("1, 2, 3중 하나를 입력하세요")
-                continue
-            break
-        except ValueError:
-            print("정수를 입력하세요")
-    for i in range(play):
-        num += 1
-        if num > 31:
-            print("playerB win!")
-            break
-        print(f"playerA:{num}")
+    if cnt % 2 == 0:
+        brGame('A', 'B')
+    else:
+        brGame('B', 'A')
     if num > 31:
             break
-    while True:
-        try:
-            play = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
-            if play > 3 or play < 1:
-                print("1, 2, 3중 하나를 입력하세요")
-                continue
-            break
-        except ValueError:
-            print("정수를 입력하세요")
-    for i in range(play):
-        num += 1
-        if num > 31:
-            print("playerA win!")
-            break
-        print(f"playerB:{num}")
-    if num > 31:
-            break
+    cnt+=1
